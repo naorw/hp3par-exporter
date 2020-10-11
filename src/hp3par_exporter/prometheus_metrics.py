ecf419be901717c42568529d4295ebdd5db5e2ef
Naor W <40463275+naorw@users.noreply.github.com>
2020-10-11 15:23:54 +0300

    Update prometheus_metrics.py
    updating with support for volume list with sizes

--- src/hp3par_exporter/prometheus_metrics.py
+++ src/hp3par_exporter/prometheus_metrics.py
@@ -10,5 +10,8 @@ gauge_hp3par_free_capacity_mib = Gauge('hp3par_freeCapacityMiB',
                                        'Total free capacity in MiB', ["id", "hp3par_name"])
 gauge_hp3par_failed_capacity_mib = Gauge('hp3par_failedCapacityMiB',
                                          'Total failed capacity in MiB', ["id", "hp3par_name"])
-
+gauge_hp3par_volumes = Gauge('hp3par_volumes',
+                                         'Total failed capacity in MiB', ["id", "hp3par_name"])
+gauge_hp3par_volume_used = Gauge('hp3par_volume_used',
+                                         'Volume used size :', ["id", "hp3par_name"])
 
